64ab776d3fba983ae1c62165510d125791fa56eb
yycquant0812 <xherobruce@gmail.com>
2025-10-16 14:26:26 +0800

    hw5
    Added a star pyramid and multiplication table to main function.

--- test1/main.cpp
+++ test1/main.cpp
@@ -1,5 +1,28 @@
- #include <iostream>
- int main() {
-   std::cout << "Hello, World!\n"; // 螢幕印出字串，\n 是換行
-   return 0;                        // 回傳 0 代表「程式順利結束」
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 星號金字塔
+    for (int i = 1; i <= 6; ++i) {
+        for (int j = 1; j <= i; ++j) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    cout << endl; // 分隔用
+
+    // 九九乘法表
+    int i = 1, j;
+    while (i <= 9) {
+        j = 1;
+        while (j <= 9) {
+            cout << i << " * " << j << " = " << i * j << "\t";
+            j += 1;
+        }
+        cout << endl;
+        i += 1;
+    }
+
+    return 0;
 }
